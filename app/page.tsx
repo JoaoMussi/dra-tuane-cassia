4f41c7b0eee35b51a8982a8d4a793e29188da8b5
Joao Mussi Gotardelo <joaogotardelo@gmail.com>
2024-05-07 08:48:49 -0300

    configuring tailwind and adding daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
 	return (
 		<>
 			<h1>Main page!</h1>
+			<button className='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'>
+				Button
+			</button>
 		</>
 	);
 }
