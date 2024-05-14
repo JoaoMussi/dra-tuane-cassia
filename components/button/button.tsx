07c3e2a49a18bc06d57af9fe53da7c6f710f6dec
Joao Mussi Gotardelo <joaogotardelo@gmail.com>
2024-05-14 07:52:19 -0300

    feat: implement button component

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -3,11 +3,33 @@ const styles = {
 };
 
 export default function Button({
-	label = 'Label',
-	variant = 'primary',
+	label,
+	showArrow = true,
+	children,
 }: {
 	label?: string;
-	variant?: string;
+	showArrow?: boolean;
+	children?: React.ReactNode;
 }) {
-	return <button className={`btn btn-outline`}>{label}</button>;
+	return (
+		<button className={`btn btn-primary btn-outline rounded-full`}>
+			{label}
+			{children}
+			{showArrow ? (
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					fill='none'
+					viewBox='0 0 24 24'
+					strokeWidth={1.5}
+					stroke='currentColor'
+					className='w-6 h-6'>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						d='M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3'
+					/>
+				</svg>
+			) : null}
+		</button>
+	);
 }
