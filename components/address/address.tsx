8e8f5bb0f7ff1bdafb57a163c8e1842489d98394
Joao Mussi Gotardelo <joaogotardelo@gmail.com>
2024-07-09 19:19:33 -0300

    fix: remove section from global css and fix nested paragraph

--- components/address/address.tsx
+++ components/address/address.tsx
@@ -9,13 +9,16 @@ export default function TuaneAddress() {
 			sectionName='Endereço'
 			regularWidth={false}>
 			<TuaneParagraph>
-				<p className='text-md font-bold'>Clínica integrada Dukeze</p>
+				<span className='text-md font-bold'>
+					Clínica integrada Dukeze
+				</span>
+				<br />
 				Rua Emílio Artmann, 130 Bairro América, Joinville/SC
 			</TuaneParagraph>
 			<iframe
 				title='Endereço da Clinica Integrada Dukeze'
-				width='600'
-				height='450'
+				width='450'
+				height='400'
 				style={{ border: 0 }}
 				src='https://www.google.com/maps/embed/v1/place?q=place_id:ChIJrXkgFyKx3pQRQwOWs4wIR70&key=AIzaSyAtQXQiEtwbzpojuZdUUw60GVoiPPKNG9o'></iframe>
 		</TuaneInfoSection>
