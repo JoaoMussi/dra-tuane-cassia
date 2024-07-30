400ac7e70ec54556ae606c90b81cf7adc7c521ff
Joao Mussi Gotardelo <joaogotardelo@gmail.com>
2024-07-30 07:07:28 -0300

    feat: add loading in TuaneButton

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -5,12 +5,14 @@ import Link from 'next/link';
 
 interface TuaneButtonProps extends ButtonProps {
 	variant?: string;
+	loading?: boolean;
 	outline?: boolean;
 	showArrow?: boolean;
 }
 
 export default function TuaneButton({
 	variant = 'primary',
+	loading = false,
 	outline = true,
 	showArrow = true,
 	children,
@@ -18,28 +20,42 @@ export default function TuaneButton({
 	externalLink = false,
 	onClick,
 }: TuaneButtonProps) {
+	let buttonClass = `btn btn-${variant} rounded-full`;
+
+	if (outline) {
+		buttonClass += ' btn-outline';
+	}
+
+	if (loading) {
+		buttonClass += ' btn-disabled';
+	}
+
 	let buttonElement = (
 		<button
-			className={`${mansory.className} btn btn-${variant} ${
-				outline ? 'btn-outline' : null
-			} rounded-full`}
+			className={`${mansory.className} ${buttonClass}`}
 			onClick={onClick}>
-			{children}
-			{showArrow ? (
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					fill='none'
-					viewBox='0 0 24 24'
-					strokeWidth={1.5}
-					stroke='currentColor'
-					className='w-6 h-6'>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						d='M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3'
-					/>
-				</svg>
-			) : null}
+			{loading ? (
+				<span className='loading loading-spinner'></span>
+			) : (
+				<>
+					{children}
+					{showArrow ? (
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							strokeWidth={1.5}
+							stroke='currentColor'
+							className='w-6 h-6'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								d='M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3'
+							/>
+						</svg>
+					) : null}
+				</>
+			)}
 		</button>
 	);
 
