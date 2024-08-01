e09ec16b6ce16408ab9dbac5f9bd12a3dedf10f6
Joao Mussi Gotardelo <joaogotardelo@gmail.com>
2024-08-01 07:04:01 -0300

    fix: change facebook link

--- lib/links.ts
+++ lib/links.ts
@@ -1,5 +1,5 @@
 export enum TuaneLinks {
-	FACEBOOK = 'https://www.facebook.com/biotuanecassia',
+	FACEBOOK = 'https://www.facebook.com/profile.php?id=61561860387270',
 	INSTAGRAM = 'https://www.instagram.com/dra.tuanecassia/',
 	WHATSAPP = 'https://api.whatsapp.com/send?phone=5547996699607',
 }
