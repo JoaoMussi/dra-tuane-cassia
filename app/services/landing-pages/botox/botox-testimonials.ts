f73a8207a126c99712c715247bde5c5abcf699b5
João M. Gotardelo <joaogotardelo@gmail.com>
2024-11-08 14:16:37 -0300

    feat(botox-lp): creating testimonials configuration files

---
+++ app/services/landing-pages/botox/botox-testimonials.ts
@@ -0,0 +1,28 @@
+import { CardProps } from 'interfaces';
+
+export const botoxTestimonials: CardProps[] = [
+	{
+		title: 'Hannah',
+		hideAction: true,
+		imageAttrs: {
+			src: '/images/testimonials/Hannah.jpg',
+			alt: 'Depoimento e apresentação de resultados da paciente Hannah',
+		},
+	},
+	{
+		title: 'Roseane',
+		hideAction: true,
+		imageAttrs: {
+			src: '/images/testimonials/Roseane.jpg',
+			alt: 'Depoimento e apresentação de resultados da paciente Roseane',
+		},
+	},
+	{
+		title: 'Kênia',
+		hideAction: true,
+		imageAttrs: {
+			src: '/images/testimonials/Kenia.jpg',
+			alt: 'Depoimento e apresentação de resultados da paciente Kênia',
+		},
+	},
+];
