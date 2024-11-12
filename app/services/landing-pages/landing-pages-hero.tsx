76cda5b2ceb477ecf904ff27cad437314ab753bb
João M. Gotardelo <joaogotardelo@gmail.com>
2024-11-12 15:12:08 -0300

    fix(botox-lp): add mobile responsiveness

--- app/services/landing-pages/landing-pages-hero.tsx
+++ app/services/landing-pages/landing-pages-hero.tsx
@@ -1,30 +1,11 @@
-import { TuaneButton } from 'components';
+import { TuaneButton, TuaneParagraph } from 'components';
 import { WhatsMessages, whatsLinkWithMessage } from 'lib';
 import Image from 'next/image';
 
 export default function TuaneLandingPagesHero() {
 	return (
-		<div className='hero h-[620px] flex justify-center items-center'>
-			<div className='hero-content flex-col md:flex-row-reverse gap-32'>
-				<div className='diff aspect-[3/4]'>
-					<div className='diff-item-1'>
-						<Image
-							unoptimized={true}
-							src='/images/depois-diff.jpg'
-							alt='Fotografia retirada depois da aplicação de botox na paciente Hannah'
-							width={500}
-							height={300}></Image>
-					</div>
-					<div className='diff-item-2'>
-						<Image
-							unoptimized={true}
-							src='/images/antes-diff.jpg'
-							alt='Fotografia retirada antes da aplicação de botox na paciente Hannah'
-							width={500}
-							height={300}></Image>
-					</div>
-					<div className='diff-resizer'></div>
-				</div>
+		<div className='hero md:h-[620px] flex justify-center items-center'>
+			<div className='hero-content flex-col md:flex-row gap-16'>
 				<div className='text-center text-neutral-content flex flex-col items-center gap-12 pt-[120px]'>
 					<Image
 						src='/logos/logotipo-sem-icone-marrom.png'
@@ -41,13 +22,36 @@ export default function TuaneLandingPagesHero() {
 							e um ótimo desconto exclusivo para você.
 						</p>
 						<TuaneButton
-							href={whatsLinkWithMessage(
-								WhatsMessages.APPOINTMENT
-							)}
+							href={whatsLinkWithMessage(WhatsMessages.LP_BOTOX)}
 							outline={false}
 							externalLink>
 							Agendar avaliação
 						</TuaneButton>
+					</div>
+				</div>
+
+				<div className='text-center'>
+					<TuaneParagraph>
+						Arraste o círculo para descobrir o poder do Botox.
+					</TuaneParagraph>
+					<div className='diff aspect-[3/4]'>
+						<div className='diff-item-1'>
+							<Image
+								unoptimized={true}
+								src='/images/depois-diff.jpg'
+								alt='Fotografia retirada depois da aplicação de botox na paciente Hannah'
+								width={500}
+								height={300}></Image>
+						</div>
+						<div className='diff-item-2'>
+							<Image
+								unoptimized={true}
+								src='/images/antes-diff.jpg'
+								alt='Fotografia retirada antes da aplicação de botox na paciente Hannah'
+								width={500}
+								height={300}></Image>
+						</div>
+						<div className='diff-resizer'></div>
 					</div>
 				</div>
 			</div>
