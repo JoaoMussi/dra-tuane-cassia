769b5ac00a4dd9b3b96f5776bab29d1c48b46dd9
João M. Gotardelo <joaogotardelo@gmail.com>
2024-11-18 18:12:16 -0300

    feat(botox-lp): improve hero section

--- app/services/landing-pages/landing-pages-hero.tsx
+++ app/services/landing-pages/landing-pages-hero.tsx
@@ -1,12 +1,17 @@
-import { TuaneButton, TuaneParagraph } from 'components';
+import { TuaneButton } from 'components';
 import { WhatsMessages, whatsLinkWithMessage } from 'lib';
 import Image from 'next/image';
 
 export default function TuaneLandingPagesHero() {
 	return (
-		<div className='hero md:h-[620px] flex justify-center items-center'>
-			<div className='hero-content flex-col md:flex-row gap-16'>
-				<div className='text-center text-neutral-content flex flex-col items-center gap-12 pt-[120px]'>
+		<div
+			className='hero'
+			style={{
+				backgroundImage: `url('/images/tuane-holding-toxina.png')`,
+			}}>
+			<div className='hero-overlay py-20 bg-white bg-opacity-100 md:bg-opacity-30'></div>
+			<div className='hero-content md:py-20 flex-col justify-start md:flex-row gap-16'>
+				<div className='md:w-[45%] text-neutral-content flex flex-col items-center gap-12 pt-[120px]'>
 					<Image
 						src='/logos/logotipo-sem-icone-marrom.png'
 						alt='Logotipo Dra Tuane Cássia'
@@ -14,44 +19,26 @@ export default function TuaneLandingPagesHero() {
 						height={300}></Image>
 					<div>
 						<h1 className='text-4xl font-bold text-black'>
-							Descubra a beleza de uma pele renovada com o Botox,
-							sob medida para você!
+							A toxina butolínica como cuidado pessoal para uma
+							pele rejuvenescida e natural
 						</h1>
-						<p className='m-5 text-lg text-black'>
-							Rejuvenescimento facial com naturalidade, segurança
-							e um ótimo desconto exclusivo para você.
+						<p className='my-5 text-lg text-black'>
+							Sabe aquelas rugas e linhas de expressão que
+							aparecem com o tempo? Com a nossa ajuda, você pode
+							suavizar tudo de forma bem rápida e sem precisar de
+							cirurgia. O melhor de tudo é que os resultados
+							começam a aparecer em poucos dias e ficam super
+							naturais, sem mudar suas características. Se você
+							está pensando em dar uma rejuvenescida de maneira
+							discreta, a toxina butolínica pode ser a solução
+							ideal!
 						</p>
 						<TuaneButton
-							href={whatsLinkWithMessage(WhatsMessages.LP_BOTOX)}
+							href={whatsLinkWithMessage(WhatsMessages.LP_TOXIN)}
 							outline={false}
 							externalLink>
 							Agendar avaliação
 						</TuaneButton>
-					</div>
-				</div>
-
-				<div className='text-center'>
-					<TuaneParagraph>
-						Arraste o círculo para descobrir o poder do Botox.
-					</TuaneParagraph>
-					<div className='diff aspect-[3/4]'>
-						<div className='diff-item-1'>
-							<Image
-								unoptimized={true}
-								src='/images/depois-diff.jpg'
-								alt='Fotografia retirada depois da aplicação de botox na paciente Hannah'
-								width={500}
-								height={300}></Image>
-						</div>
-						<div className='diff-item-2'>
-							<Image
-								unoptimized={true}
-								src='/images/antes-diff.jpg'
-								alt='Fotografia retirada antes da aplicação de botox na paciente Hannah'
-								width={500}
-								height={300}></Image>
-						</div>
-						<div className='diff-resizer'></div>
 					</div>
 				</div>
 			</div>
