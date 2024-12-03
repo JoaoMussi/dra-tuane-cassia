50db63f24574c0f98e4076d0cbe0d14b5a1a3c12
João M. Gotardelo <joaogotardelo@gmail.com>
2024-12-03 15:34:24 -0300

    chore: add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import { montserrat } from './styles/fonts';
 import './styles/globals.css';
 import './styles/reset.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
 	title: {
@@ -25,6 +26,20 @@ export default function RootLayout({
 				<TuaneNavBar />
 				<div className='flex-1'>{children}</div>
 				<TuaneFooter />
+
+				<Script
+					id='microsoft-clarity-init'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+							`,
+					}}
+				/>
 			</body>
 			<GoogleAnalytics gaId='G-83E4PVFDZ4' />
 			<GoogleAnalytics gaId='AW-16774961383' />
